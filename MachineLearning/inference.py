572f7e2c457a3de0dc66ebbebf6e23a332473ac3
HemanthKumar M <hemanthkumarava@gmail.com>
2025-09-05 11:42:36 +0530

    added inference.py

---
+++ MachineLearning/inference.py
@@ -0,0 +1,21 @@
+import pickle
+import pandas as pd
+
+
+df = pd.DataFrame(
+    [['M', '51-55', 7, 'B', "3", 1, 1]],
+    columns=[
+        'Gender',
+        'Age',
+        'Occupation',
+        'City_Category',
+        'Stay_In_Current_City_Years',
+        'Marital_Status',
+        'Product_Category_1'
+    ]
+)
+
+with open(r"C:\Users\mhema\OneDrive\Desktop\DataScience\MachineLearning\regression.pkl", "rb") as f:
+    pipeline = pickle.load(f)
+
+print(pipeline.predict(df))
